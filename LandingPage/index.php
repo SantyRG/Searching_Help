4f34dbe856a29c9725a65f92eea7eac6d6badca0
SantyRG <sroldan90@misena.edu.co>
2017-02-14 19:02:53 -0500

    first version

---
+++ LandingPage/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Searching Help</title>
+    <link rel="stylesheet" href="assets/stylesheet/materialize/css/materialize.min.css">
+  </head>
+  <body>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  </body>
+</html>
